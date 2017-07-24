75e0136c50506d90fda94dbb8e604c9fb6085802
Diogo <diogolumao@gmail.com>
2017-07-23 21:30:59 -0300

    envio de arquivos

---
+++ Desktop/diogo-vianet/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+  $('#modal1').modal('open');
+  $('#modal1').modal('close');
+});
+ $(document).ready(function(){
+  $('.parallax').parallax();
+});
